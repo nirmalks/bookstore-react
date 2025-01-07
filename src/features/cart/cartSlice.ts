7895f71573be7171652029e07921ca5447582e3e
nirmalks <kagosekar@gmail.com>
2025-01-07 20:34:59 +0530

    add theme toggle functionality and cart icon base in navbar

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,17 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 50,
+  tax: 0,
+  orderTotal: 0,
+};
 
-const initialState = {
-  user: {},
-  theme: {},
+const getCartFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('cart')) || defaultState;
 };
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState,
+  initialState: getCartFromLocalStorage(),
   reducers: {
+    addItem: () => {
 
+    },
+    clearCart: () => {
+
+    },
+    removeItem: () => {
+
+    },
+    editItem: () => {
+
+    }
   },
 });
 
+export const { addItem, clearCart, removeItem, editItem } = cartSlice.actions;
 export default cartSlice.reducer;
